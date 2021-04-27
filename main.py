50dd904b7f8b90354ff13ec7cdfed21e42725ea1
Pikayue11 <11812715@mail.sustech.edu.cn>
2021-04-27 22:37:34 +0800

    no threads

--- main.py
+++ main.py
@@ -58,7 +58,7 @@ left_column = [
                [i1,f1],
                # [t3,queryLimit],
                [Check_image,Comfirm_all, Stop, Clear, Quit],
-               [s1],
+               # [s1],
                # [t5],
                # [pb],
                [t6]
@@ -75,61 +75,56 @@ layout1 = [[sg.Column(left_column),
 window1 = sg.Window('Our tool box', layout1)
 win2_active = False
 
+while True:
+    event1, values1 = window1.read(timeout=100)
 
+    if event1 in (None, '-quit-'):  # click quit
+        for i in threads:
+            tc._async_raise(i.ident, SystemExit)
+        break
 
-def mainWindow():
-    while True:
-        event1, values1 = window1.read(timeout=100)
+    if event1 in (None, '-stop-'):  # click quit
+        for i in threads:
+            tc._async_raise(i.ident, SystemExit)
+        threads = []
+        window1['-t6-'].update('States: free          ')
+        window1['-ori_image-'].update()
+        window1['-adv_image-'].update()
+        print('stopped')
 
-        if event1 in (None, '-quit-'):  # click quit
-            for i in threads:
-                tc._async_raise(i.ident, SystemExit)
-            break
+    if event1 in (None, '-clear-'):
+        window1['-output-'].update(value='')
 
-        if event1 in (None, '-stop-'):  # click quit
-            for i in threads:
-                tc._async_raise(i.ident, SystemExit)
-            threads = []
-            window1['-t6-'].update('States: free          ')
-            window1['-ori_image-'].update()
-            window1['-adv_image-'].update()
-            print('stopped')
-
-        if event1 in (None, '-clear-'):
-            window1['-output-'].update(value='')
-
-        if not win2_active and event1 == '-ci-':  # click check image
-            path = window1['-ImagePath-'].get()
-            if (path == ''):
-                print('path is empty')
+    if not win2_active and event1 == '-ci-':  # click check image
+        path = window1['-ImagePath-'].get()
+        if (path == ''):
+            print('path is empty')
+        else:
+            II = gui.ImageInfo(window1['-db-'].get())
+            ori_newPath = gui.savePng(path, II.width, II.height)
+            if ori_newPath == '':
+                print('Warning! Please enter a correct image path!')
             else:
-                II = gui.ImageInfo(window1['-db-'].get())
-                ori_newPath = gui.savePng(path, II.width, II.height)
-                if ori_newPath == '':
-                    print('Warning! Please enter a correct image path!')
-                else:
-                    ori_images = gui.getImage(ori_newPath)
-                    ori_label_id = gui.getLabel(ori_images)
-                    ori_label_name = II.labels[int(ori_label_id)]
-                    print("The label of the original image is", ori_label_name)
-                    window1['-ori_image-'].update(size=(II.width, II.height), filename=ori_newPath)
+                ori_images = gui.getImage(ori_newPath)
+                ori_label_id = gui.getLabel(ori_images)
+                ori_label_name = II.labels[int(ori_label_id)]
+                print("The label of the original image is", ori_label_name)
+                window1['-ori_image-'].update(size=(II.width, II.height), filename=ori_newPath)
 
+    if not win2_active and event1 == '-ca-':  # click comfirm all
 
-        if not win2_active and event1 == '-ca-':  # click comfirm all
+        if window1['-t6-'].get()[8:12] == 'free':
+            window1['-t6-'].update('States: running   ')
+            t1 = threading.Thread(target=gui.getAdvPath, args=(ori_images, II, window1,))
+            threads.append(t1)
+            t1.start()
+            t2 = threading.Thread(target=gui.updateRunning, args=(window1,))
+            threads.append(t2)
+            t2.start()
 
-            if window1['-t6-'].get()[8:12] == 'free':
-                window1['-t6-'].update('States: running   ')
-                t1 = threading.Thread(target=gui.getAdvPath, args=(ori_images, II, window1,))
-                threads.append(t1)
-                t1.start()
-                t2 = threading.Thread(target=gui.updateRunning, args=(window1,))
-                threads.append(t2)
-                t2.start()
+        else:
+            print('There is someing running, please wait')
 
-            else:
-                print('There is someing running, please wait')
+window1.close()
 
-    window1.close()
-
-mainWindow()
 
