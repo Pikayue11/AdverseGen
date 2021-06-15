c3b28525e1b6680dee3e29aa1e265368ea43df13
sxcz <11812714@mail.sustech.edu.cn>
2021-06-15 15:32:28 +0800

    add log module

---
+++ backEnd/toolkit/log_management.py
@@ -0,0 +1,150 @@
+from .distances import *
+from .log_util import *
+import logging
+import sys
+
+logger = logging.getLogger(__name__)
+logging.basicConfig(stream=sys.stdout,
+                    level=logging.INFO,
+                    format='%(asctime)s %(message)s',
+                    datefmt='%Y/%m/%d %H:%M:%S')
+handler = logging.FileHandler("log.txt")
+handler.setLevel(logging.INFO)
+formatter = logging.Formatter('%(asctime)s %(message)s')
+handler.setFormatter(formatter)
+logger.addHandler(handler)
+logger.addHandler(logging.StreamHandler(sys.stdout))
+
+
+class LogManagement():
+    def __init__(self,
+                 img,
+                 oriLabel,
+                 modelwithName,  # [model, name]
+                 norm,
+                 target=None,
+                 databaseName='CIFAR-10',
+                 imageName='wkyTest',
+                 extention='png'):
+        self.imageName = imageName
+        self.extention = extention
+        self.model = modelwithName[0]
+        self.modelName = modelwithName[1]
+        self.databaseName = databaseName
+        self.oriLabel = oriLabel
+        self.oriLabelStr = mapLabel(self.databaseName, self.oriLabel)
+        self.target = None
+        if target is None:
+            self.isTarget = False
+        else:
+            self.isTarget = True
+            self.target = target[0]
+        self.norm = norm
+        self.img = img
+        self.logStart()
+
+    def logStart(self):
+        attackType = 'target' if self.isTarget else 'non-target'
+        targetMsg = 'Target label: %s' % (mapLabel(self.databaseName, self.target)) if self.isTarget else ''
+        logger.info('\n*** \n    Launching an attack to image %s\n' % (self.imageName) +
+                    '    Norm: %s\n' % (self.norm) +
+                    '    Attack type: %s' % (attackType) +
+                    '    Original label: %s\n    ' % (self.oriLabelStr) + targetMsg +
+                    '\n    Model: %s\n' % (self.modelName) +
+                    '    Database: %s\n' % (self.databaseName) +
+                    '***')
+        print('\n*** \n    Launching an attack to image %s\n' % (self.imageName) +
+              '    Norm: %s\n' % (self.norm) +
+              '    Attack type: %s' % (attackType) +
+              '    Original label: %s\n    ' % (self.oriLabelStr) + targetMsg +
+              '\n    Model: %s\n' % (self.modelName) +
+              '    Database: %s\n' % (self.databaseName) +
+              '***')
+        saveImage(self.img[0], self.imageName, self.extention, 'ori')
+        saveImage(self.img[0] - self.img[0], self.imageName, self.extention, 'diff')
+        saveImage(self.img[0], self.imageName, self.extention, 'adv')
+
+    def imgUpdate(self, advImg, iter=-1):
+
+        if self.isTarget:
+            self.updateTarget(advImg, iter)
+        else:
+            self.updateNonTarget(advImg, iter)
+        saveImage(advImg[0], self.imageName, self.extention, 'adv')
+        saveImage(get_diff(self.img[0], advImg[0]), self.imageName, self.extention, 'diff')
+
+    def updateTarget(self, advImg, iter):
+        cons = getCons(self.model, advImg[0])
+        normValue = getNormValue(self.img[0], advImg[0], self.norm)
+        if (iter <= 0):
+            logger.info('origin_con: %.4f, target_con: %.4f, %s: %.3f' % (
+            cons[int(self.oriLabel)], cons[int(self.target)], self.norm, normValue))
+            print('origin_con: %.4f, target_con: %.4f, %s: %.3f' % (
+            cons[int(self.oriLabel)], cons[int(self.target)], self.norm, normValue))
+        else:
+            logger.info('[Iter: %7d] origin_con: %.4f, target_con: %.4f, %s: %.3f' % (
+            iter, cons[int(self.oriLabel)], cons[int(self.target)], self.norm, normValue))
+            print('[Iter: %7d] origin_con: %.4f, target_con: %.4f, %s: %.3f' % (
+            iter, cons[int(self.oriLabel)], cons[int(self.target)], self.norm, normValue))
+
+    def updateNonTarget(self, advImg, iter):
+        cons = getCons(self.model, advImg[0])
+        normValue = getNormValue(self.img[0], advImg[0], self.norm)
+        if (iter <= 0):
+            logger.info('origin_con: %.4f, %s: %.3f' % (cons[self.oriLabel], self.norm, normValue))
+            print('origin_con: %.4f, %s: %.3f' % (cons[self.oriLabel], self.norm, normValue))
+        else:
+            logger.info('[Iter: %7d] origin_con: %.4f, %s: %.3f' % (iter, cons[self.oriLabel], self.norm, normValue))
+            print('[Iter: %7d] origin_con: %.4f, %s: %.3f' % (iter, cons[self.oriLabel], self.norm, normValue))
+
+    def logEnd(self, advImg):
+        if self.isTarget:
+            self.endTarget(advImg)
+        else:
+            self.endNonTarget(advImg)
+        saveImage(advImg[0], self.imageName, self.extention, 'adv')
+        saveImage(get_diff(self.img[0], advImg[0]), self.imageName, self.extention, 'diff')
+
+    def endTarget(self, advImg):
+        cons = getCons(self.model, advImg[0])
+        normValue = getNormValue(self.img[0], advImg[0], self.norm)
+        success = 'successed' if cons.argmax() == self.target else 'failed'
+        logger.info('\n*** \n    End of the attack, it %s\n'
+
+                    '    Origin_label: %s\n'
+                    '    Target_label: %s\n'
+                    '    Current label: %s\n'
+                    '    %s: %.3f\n'
+                    '***' % (success, self.oriLabelStr,
+                             mapLabel(self.databaseName, self.target),
+                             mapLabel(self.databaseName, cons.argmax()),
+                             self.norm, normValue))
+        print('\n*** \n    End of the attack, it %s\n'
+
+              '    Origin_label: %s\n'
+              '    Target_label: %s\n'
+              '    Current label: %s\n'
+              '    %s: %.3f\n'
+              '***' % (success, self.oriLabelStr,
+                       mapLabel(self.databaseName, self.target),
+                       mapLabel(self.databaseName, cons.argmax()),
+                       self.norm, normValue))
+
+    def endNonTarget(self, advImg):
+        cons = getCons(self.model, advImg[0])
+        normValue = getNormValue(self.img[0], advImg[0], self.norm)
+        success = 'successed' if cons.argmax() != self.oriLabel else 'failed'
+        logger.info('\n*** \n    End of the attack, it %s\n'
+                    '    Origin_label: %s\n'
+                    '    Current label: %s\n'
+                    '    %s: %.3f\n'
+                    '***' % (success, self.oriLabelStr,
+                             mapLabel(self.databaseName, cons.argmax()),
+                             self.norm, normValue))
+        print('\n*** \n    End of the attack, it %s\n'
+              '    Origin_label: %s\n'
+              '    Current label: %s\n'
+              '    %s: %.3f\n'
+              '***' % (success, self.oriLabelStr,
+                       mapLabel(self.databaseName, cons.argmax()),
+                       self.norm, normValue))
---
+++ backEnd/toolkit/log_util.py
@@ -0,0 +1,104 @@
+import numpy as np
+from PIL import Image
+
+def saveImage(advImage, filename, extension, type): ## type: ori, adv, diff
+    folder_name = './tmp/'
+    newName = folder_name + type + '_' + filename + '.' + extension
+    im = denormalize_images(advImage, type)
+    im = Image.fromarray(im)
+    im.save(newName)
+
+def denormalize_images(image, type):
+    image = np.array(image)
+    if type != 'diff':
+        if(np.max(image) <= 1):
+            image *= 255
+    return image.astype(np.uint8)
+
+def getFileNameAndExtension(path):
+    slashIndex = path.rindex('/')
+    if(slashIndex == -1 or slashIndex+1 == len(path)):
+        print('Please choose a right file path')
+        return None, None
+    extensionIndex = path.rindex('.')
+    if (extensionIndex == -1 or extensionIndex + 1 == len(path) or extensionIndex == slashIndex + 1):
+        print('Please choose a right file path with a correct extension')
+        return None, None
+    extention = path[extensionIndex + 1:]
+    fileName = path[slashIndex + 1:extensionIndex]
+    return fileName, extention
+
+
+def get_diff(img1, img2): #[32 ,32 ,3]
+    img1 = np.array(img1)
+    img2 = np.array(img2)
+    diff_img = img1 - img2
+    diff_img = diff_img - np.min(diff_img)
+    if np.max(diff_img) > 0:
+        diff_img = diff_img / np.max(diff_img)
+        diff_img *= 255
+    diff_img = np.array(diff_img)
+    return diff_img.astype(np.uint8)
+
+
+
+# three ugly function
+
+
+
+def mapLabel(database, index):
+    cifar_10_labels = ['plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck']
+    cifar_10_labels = np.array(cifar_10_labels)
+
+    if database == 'CIFAR-10':
+        return cifar_10_labels[int(index)]
+    else:
+        print("mapLabel, not cifar 10, in log_util")
+        return ''
+
+from skimage.metrics import _structural_similarity as ss
+def getNormValue(img, advImg, norm):
+    if norm == 'SSIM':
+        return compute_ssim(img, advImg)
+    return -1
+
+def compute_ssim(img1, img2): # [32,32,3]
+    img1 = img1.copy().astype(np.uint8)
+    img2 = img2.copy().astype(np.uint8)
+    if img1.shape[2] > 1:
+        return ss.structural_similarity(img1, img2, multichannel=True)
+    else:
+        return ss.structural_similarity(img1, img2, multichannel=False)
+
+
+import torch
+from torchvision import transforms
+transform = transforms.Compose([
+    transforms.ToTensor(),
+    transforms.Normalize(
+        mean=[0.4914, 0.4822, 0.4465],
+        std=[0.2471, 0.2435, 0.2616]),
+])
+device = ('cuda' if torch.cuda.is_available() else 'cpu')
+def getCons(model, img):
+    output = model(np.expand_dims(img / 255, axis=0))
+    output = torch.nn.Softmax(dim=1)(torch.from_numpy(output)).squeeze().numpy()
+
+    return output
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
